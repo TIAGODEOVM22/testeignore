ba38b5252461ccdc7615f0e0a741f7d9b3a9ed9a
Tiago de Oliveira <tiagodeoliveiravillalva@gmail.com>
2024-07-22 16:35:08 -0300

    docs: add openapi docs

--- user-service-api/src/main/java/br/com/tiago/user_service_api/config/OpenApiConfig.java
+++ user-service-api/src/main/java/br/com/tiago/user_service_api/config/OpenApiConfig.java
@@ -3,25 +3,23 @@ package br.com.tiago.user_service_api.config;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.actuate.web.mappings.HandlerMethodDescription;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+
 @Configuration
-public class OpenApiConfig {
+public class OpenAPIConfig {
 
     @Bean
-    public OpenAPI custonOpenApi(
+    public OpenAPI custonOpenAPI(
             @Value("${springdoc.openapi.title}") final String title,
             @Value("${springdoc.openapi.description}") final String description,
             @Value("${springdoc.openapi.version}") final String version
     ){
-        return new OpenAPI()
-                .info(
-                        new Info()
-                        .title(title)
-                        .description(description)
-                        .version(version)
-                );
+        return new OpenAPI().info(new Info()
+                            .title(title)
+                            .description(description)
+                            .version(version)
+                     );
     }
 }
